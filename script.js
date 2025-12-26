2c396fcaef7b569ccc2b6021cd0ad0a9e5c29feb
Reza Adamir <stu238357@mail.uni-kiel.de>
2025-12-26 18:44:59 +0100

    Javascript wurde implemntiert

---
+++ script.js
@@ -0,0 +1,15 @@
+const mainMenu = document.querySelector(".mainMenu");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show(){
+    mainMenu.style.display = "flex"
+    mainMenu.style.top = "0"
+}
+
+function close(){
+    closeMenu.style.top = "-150%"
+}
